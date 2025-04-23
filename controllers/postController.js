28902fb5f56e7be26221a983b2f504b5e9e8694f
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-23 17:39:14 +0200

    [UPD] aggiunti altri comandi

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,3 +10,13 @@ function index(req,res){
 function show(req,res){
     res.send(`post con ID ${req.params.id}`);
 }
+
+// store
+function store(req,res){
+    res.send('Inserimento di un nuovo post');
+}
+
+// update
+function update (req,res){
+    res.send(`modifica totale del post con ID ${req.params.id}`);
+}
